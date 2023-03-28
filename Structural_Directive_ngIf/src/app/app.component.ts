d7c8cd6a33d36ba1bfaea9c7ec9d202a915d524e
Devansh Raval <pofgod875@gmail.com>
2023-03-28 12:10:02 +0530

    Task Done

--- Structural_Directive_ngIf/src/app/app.component.ts
+++ Structural_Directive_ngIf/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , ElementRef , Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private e : ElementRef ,private r : Renderer2){}
+
   title = 'Structural_Directive_ngIf';
+  showValue : boolean = false;
+  showDiv(){
+  
+    this.showValue = true;
+ 
+  }
+  hideDiv(){
+     
+      this.showValue = false;
+ 
+  }
 }
+ 
