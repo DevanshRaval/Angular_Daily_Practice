23fb29498cd72831278f33df9b7d9c5291ec71ad
Devansh Raval <pofgod875@gmail.com>
2023-03-28 11:32:06 +0530

    Task Done

--- HostBinding_HostListener/src/app/app.component.ts
+++ HostBinding_HostListener/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HostBinding_';
+  backgroundColor : string = 'grey'
+
 }
--- HostBinding_HostListener/src/app/my-dir.directive.ts
+++ HostBinding_HostListener/src/app/my-dir.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive , Input , ElementRef , HostListener , HostBinding, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appMyDir]'
 })
 export class MyDirDirective {
 
-  constructor() { }
+  constructor(private element : ElementRef , private renderer : Renderer2) { }
+   @HostBinding('style.color') @Input() receivedColorValue :string = ''; 
+
+  @HostListener('mouseover') over(){
+    this.renderer.setStyle(this.element.nativeElement,'backgroundColor',this.receivedColorValue)
+    this.renderer.setStyle(this.element.nativeElement,'color','white')
+    this.renderer.setStyle(this.element.nativeElement,'height','10vh')
+    this.renderer.setStyle(this.element.nativeElement,'transition','2s')
+    this.renderer.setStyle(this.element.nativeElement,'lineHeight','10vh')
+    
+
+  }
+
+  @HostListener('mouseleave') leave(){
+    this.renderer.setStyle(this.element.nativeElement,'backgroundColor','rgb(207, 240, 245)');
+    this.renderer.setStyle(this.element.nativeElement,'color','grey');
+    this.renderer.setStyle(this.element.nativeElement,'height','5vh');
+    this.renderer.setStyle(this.element.nativeElement,'lineHeight','5vh')
+  }
 
 }
