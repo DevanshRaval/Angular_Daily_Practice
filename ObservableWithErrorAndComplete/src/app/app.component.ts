45966bffa164f86d6e17ce5583ca9a46d0ee155f
Devansh Raval <pofgod875@gmail.com>
2023-03-29 15:54:11 +0530

    Initial Commit

--- ObservableWithErrorAndComplete/src/app/app.component.ts
+++ ObservableWithErrorAndComplete/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,22 +7,34 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'ObservableWithErrorAndComplete';
-  obs = new Observable((observer)=>{
-    setTimeout(() => {observer.next("1")}, 100);
-    setTimeout(() => {observer.next("2")}, 200);
-    setTimeout(() => {observer.next("3")}, 300);
-    // setTimeout(() => {observer.error(new Error("Something Went Wrong Please Try Again Later"))}, 400);
-    setTimeout(() => {observer.next("5")}, 400);
-    setTimeout(() => {observer.next("6")}, 500);
-    setTimeout(() => {observer.next("7")}, 600);
-    setTimeout(() => {observer.complete()}, 600);
-  });
+  // obs = new Observable((observer)=>{
+  //   setTimeout(() => {observer.next("1")}, 100);
+  //   setTimeout(() => {observer.next("2")}, 200);
+  //   setTimeout(() => {observer.next("3")}, 300);
+  //   // setTimeout(() => {observer.error(new Error("Something Went Wrong Please Try Again Later"))}, 400);
+  //   setTimeout(() => {observer.next("5")}, 400);
+  //   setTimeout(() => {observer.next("6")}, 500);
+  //   setTimeout(() => {observer.next("7")}, 600);
+  //   setTimeout(() => {observer.complete()}, 600);
+  // });
+
+  // startObservable(){
+  //   this.obs.subscribe(
+  //     next=>console.log(next),
+  //     error=>console.error(error),
+  //     ()=>console.log("Observable Completed")
+  //   )
+  // }
+  array1: string[] = ["one", "two", "three", "four", "five"];
+  array2: number[] = [1, 2, 3, 4, 5, 6, 78, 9];
+  obs = of(this.array1, this.array2)
 
   startObservable(){
     this.obs.subscribe(
       next=>console.log(next),
-      error=>console.error(error),
-      ()=>console.log("Observable Completed")
+      ()=>console.log("This is for Delete"),
+      ()=>console.log("Completed")      
     )
   }
+
 }
---
+++ SubjectInRXJS_Data_Between_Component/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'SubjectInRXJS_Data_Between_Component'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('SubjectInRXJS_Data_Between_Component');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('SubjectInRXJS_Data_Between_Component app is running!');
+  });
+});
